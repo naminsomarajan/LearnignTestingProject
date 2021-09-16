c5da132871f57db812c73f40e5e45f899e766ecb
Namin <naminsomarajan@gmail.com>
2021-09-17 01:11:17 +0530

    The fist commit after using GIT tool

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function changeColour(){
+   
+    document.getElementById("header").style.backgroundColor="white";
+    document.getElementById("header").style.transition='6s';
+    document.getElementById("id1").style.color="black";
+    document.getElementById("id2").style.color="black";
+    document.getElementById("id3").style.color="black";
+    document.getElementById("id4").style.color="black";
+    document.getElementById("id5").style.color="black";
+    document.getElementById("id6").style.color="black";
+    document.getElementById("id7").style.color="black";
+    document.getElementById("id8").style.color="black";
+    document.getElementById("id9").style.color="black";
+    console.log('working')
+     
+}
